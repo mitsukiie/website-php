2f72b944e3452ae190314afaf5ea6bc7e6ba96c9
@Mitsukiie <69868961+mitsukiie@users.noreply.github.com>
2023-12-04 14:02:45 -0300

    Update produto.php

--- produto.php
+++ produto.php
@@ -161,9 +161,13 @@ if (isset($_GET['id'])) {
         echo "<option value='G'>G</option>";
         echo "</select>";
         echo "</div>";
-
+        if (!isset($_SESSION['user_id'])) {
+          echo "<form action='singup.php'>";
+          echo "<button type='submit' class='btn-preco carrinho'>Adicionar ao Carrinho</button>";
+          echo "</form>";
+      } else {
         echo "<button class='btn-preco carrinho' onclick='adicionarAoCarrinho()'>Adicionar ao Carrinho</button>";
-
+      }
         echo "<button class='btn-preco comprar'>Comprar Agora</button>";
         echo "</div>";
         echo "</div>";
